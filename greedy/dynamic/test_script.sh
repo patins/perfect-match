0a1ff9fd55bc50e9950b1cbcc63f930dcdfde0f7
Divya Gopinath <divya@LAPTOP-27SB9AS3.mit.edu>
2019-05-14 19:07:33 -0400

    Added normal_huge test

--- greedy/dynamic/test_script.sh
+++ greedy/dynamic/test_script.sh
@@ -8,3 +8,7 @@ echo "--------------------------"
 echo "Normal (big) graph"
 echo "--------------------------"
 ./main ../../data/normal_big_minus_one.mtx 6 2 1 15.496714153011233
+echo "-------------------------"
+echo "GA graph"
+echo "-------------------------"
+./main ../../data/normal_huge_minus_one.mtx 6 2 1 15.496714153011233
--- utils/make_graph.py
+++ utils/make_graph.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.sparse import coo_matrix
 from scipy.io import mmwrite
 
+
 def make_graph(graph_name, graph_size, distribution_name, **dist_kwargs):
     seed = 42
     np.random.seed(seed)
@@ -32,5 +33,5 @@ def make_bipartite_graph(graph_name, left_size, right_size, distribution_name, *
     matrix = coo_matrix((values, (row, col)), shape=(left_size+right_size, left_size+right_size))
     mmwrite('../data/{}.mtx'.format(graph_name), matrix, "Matrix generated with {} distribution {}. Seed {}".format(distribution_name, dist_kwargs, seed))
 
-make_graph("normal_small", 10, "normal", loc=5, scale=1)
+make_graph("normal_huge", 2500, "normal", loc=5, scale=1)
 #make_bipartite_graph("normal_bipartite", 100, 100, "normal", loc=5, scale=1)
