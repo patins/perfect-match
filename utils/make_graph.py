59d4e283247536113cdfdac2e88c6d63215e6ebb
Roshni Sahoo <rsahoo@mit.edu>
2019-05-16 11:31:05 -0400

    small changes

--- utils/make_graph.py
+++ utils/make_graph.py
@@ -41,18 +41,26 @@ def make_incomplete_bipartite_graph(graph_name, left_size, right_size, distribut
     row = np.append(trow, tcol)
     col = np.append(tcol, trow)
     dist_kwargs['size'] = len(trow)
-    values = np.tile(distribution(**dist_kwargs), 2)
-
-    for i in range(len(values)):
-        if np.random.uniform() < prob_delete_edge:
-            values[i] = 0
+    raw_values = distribution(**dist_kwargs) * (np.random.uniform(size=len(trow)) < prob_delete_edge)
+    values = np.tile(raw_values, 2)
     num_edges = sum([values[i] > 0 for i in range(len(values))])
     avg_degree = num_edges/(left_size + right_size)
-    print(num_edges, avg_degree)  
+
+    max_deg = 0
+    for i in range(left_size + right_size):
+        deg = 0
+        for j in values[i:i+right_size]:
+            if j> 0:
+                deg += 1
+        max_deg = max(deg, max_deg)
+    print(max_deg)
+    print(len(raw_values)) 
+    print(num_edges/2, avg_degree)  
     matrix = coo_matrix((values, (row, col)), shape=(left_size+right_size, left_size+right_size))
-    mmwrite('../data/{}.mtx'.format(graph_name), matrix, "Matrix generated with {} distribution {}. Seed {}".format(distribution_name, dist_kwargs, seed))
+#    mmwrite('../data/{}.mtx'.format(graph_name), matrix, "Matrix generated with {} distribution {}. Seed {}".format(distribution_name, dist_kwargs, seed))
 
-V = [100, 400, 500, 1000, 1500, 2000, 2500]
+make_incomplete_bipartite_graph("lognormal_incomplete_{}".format(2000), 1000, 1000, "lognormal", 0.5, mean=0, sigma=1) 
+#V = [100, 400, 500, 1000, 1500, 2000, 2500]
 
-for i in V:
-    make_incomplete_bipartite_graph("lognormal_incomplete_{}".format(i), int(i/2), int(i/2), "lognormal", 0.5, mean=0, sigma=1) 
+#for i in V:
+#    make_incomplete_bipartite_graph("lognormal_incomplete_{}".format(i), int(i/2), int(i/2), "lognormal", 0.5, mean=0, sigma=1) 
